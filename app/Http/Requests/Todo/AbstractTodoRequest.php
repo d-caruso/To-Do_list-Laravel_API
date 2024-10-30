a395076f07d42ae8ae91dd39235175ed5286e4b2
d-caruso <caruso.domenico@gmail.com>
2024-10-30 17:24:35 +0100

    [TASK] TDL-6 built API endpoint (PUT) and update method for updating a specific User's Todo; refactoring

--- app/Http/Requests/Todo/AbstractTodoRequest.php
+++ app/Http/Requests/Todo/AbstractTodoRequest.php
@@ -3,9 +3,8 @@
 namespace App\Http\Requests\Todo;
 
 use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Support\Facades\Auth;
 
-class TodoRequest extends FormRequest
+abstract class AbstractTodoRequest extends FormRequest
 {
     /**
      * Get the validation rules that apply to the request.
@@ -15,13 +14,17 @@ class TodoRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'title' => 'required|string|max:50',
+            // Ensure the user_id exists in the users table
+            'user_id' => 'required|exists:users,id',
         ];
     }
 
     public function authorize()
     {
-        // Check if the user is authenticated //and is the owner of the post
-        return request()->user() !== null;
+        // Get the user ID from the request
+        $userId = $this->user_id;
+
+        // Check if the authenticated user can access the specified user's todos
+        return (request()->user()->id === (int) $userId);
     }
 }
