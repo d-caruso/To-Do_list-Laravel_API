36c72b08977bea2df8dec01f942b4e5007727929
d-caruso <caruso.domenico@gmail.com>
2024-10-30 17:43:45 +0100

    [TASK] TDL-4 built controller, model, factory, migration, form request and seeder for the Todo; method for creating a todo; cleaned up some old code

--- app/Http/Requests/User/UpdatePasswordRequest.php
+++ app/Http/Requests/User/UpdatePasswordRequest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Requests;
+namespace App\Http\Requests\User;
 
 use Illuminate\Foundation\Http\FormRequest;
 
@@ -12,10 +12,5 @@ class UpdatePasswordRequest extends FormRequest
             'current_password' => 'required',
             'new_password' => 'required|string|min:8|confirmed|different:current_password',
         ];
-    }
-
-    public function authorize()
-    {
-        return true; // add authorization logic if needed
     }
 }
