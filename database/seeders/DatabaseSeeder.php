e889bde141addc2c77ad3ac8905dfc1c3236e1cf
d-caruso <caruso.domenico@gmail.com>
2024-10-30 17:40:15 +0100

    [TASK] TDL-4 built controller, model, factory, migration, form request and seeder for the Todo; method for creating a todo; cleaned up some old code

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Todo;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,11 +14,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        // Seed users
+        $users = User::factory()->count(3)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // Seed posts for each user
+        foreach ($users as $user) {
+            Todo::factory()->count(3)->create([
+                'user_id' => $user->id, // Associate each post with a user
+            ]);
+        }
     }
 }
